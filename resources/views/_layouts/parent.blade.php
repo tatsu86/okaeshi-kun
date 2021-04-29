0369eb56b9dee27ee26cd223fb1dc5c13459c216
TatsukiHayashi <hayashideveloper@gmail.com>
2021-04-30 00:17:22 +0900

    bootstrapをインストール

--- resources/views/_layouts/parent.blade.php
+++ resources/views/_layouts/parent.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="ja">
   <head>
+    <title>お返しくん</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <div style="background-color: steelblue; height:20px;">
       ここにヘッダーを表示する
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,9 @@
+@extends('./_layouts.parent')
+
+@section('content')
 <h1>HOME</h1>
+
+<button class="btn btn-success">ボタン</button>
 
 <div>
   <a href="{{ route('event.list') }}" class="btn btn-success">イベント</a>
@@ -11,3 +16,4 @@
 <div>
   <a href="{{ route('help.list') }}" class="btn btn-success">ヘルプ</a>
 </div>
+@endsection
