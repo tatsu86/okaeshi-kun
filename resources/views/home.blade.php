cc572257a06852a641abe8ef48cd86d07333d952
TatsukiHayashi <hayashideveloper@gmail.com>
2021-05-09 02:37:16 +0900

    ホーム画面のデザイン調整

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,42 @@
 @extends('./_layouts.parent')
 
 @section('content')
-<h1>HOME</h1>
 
-<button class="btn btn-success">ボタン</button>
+<div class="container mt-2">
+  <div class="row simple">
+    <div class="col">
+      <a class="card mb-2" href="{{ route('event.list') }}">
+        <div class="card-body text-center">
+          Events
+        </div>
+      </a>
+    </div>
 
-<div>
-  <a href="{{ route('event.list') }}" class="btn btn-success">イベント</a>
-</div>
+    <div class="col">
+      <a class="card mb-2" href="{{ route('celebrater.list') }}">
+        <div class="card-body text-center">
+          Celebraters
+        </div>
+      </a>
+    </div>
+  </div>
 
-<div>
-    <a href="{{ route('celebrater.list') }}" class="btn btn-success">祝ってくれた人</a>
-</div>
+  <div class="row simple">
+    <div class="col">
+      <a class="card mb-2" href="{{ route('help.list') }}">
+        <div class="card-body text-center">
+          Help
+        </div>
+      </a>
+    </div>
 
-<div>
-  <a href="{{ route('help.list') }}" class="btn btn-success">ヘルプ</a>
+    <div class="col">
+      <a class="card mb-2" href="{{ route('help.list') }}">
+        <div class="card-body text-center">
+          Template
+        </div>
+      </a>
+    </div>
+  </div>
 </div>
 @endsection
